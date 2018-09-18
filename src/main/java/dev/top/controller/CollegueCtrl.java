ae8372bc1988b492d2016adf5d09960cfea0f6d1
Dylan <dupont.cail@gmail.com>
2018-09-18 17:46:39 +0200

    front collegue majcollegue

--- src/main/java/dev/top/controller/CollegueCtrl.java
+++ src/main/java/dev/top/controller/CollegueCtrl.java
@@ -2,7 +2,9 @@ package dev.top.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import dev.top.entities.Collegue;
 import dev.top.repos.ColleguesRepo;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/collegues")
 public class CollegueCtrl {
 	private ColleguesRepo collegue;
@@ -29,16 +32,30 @@ public class CollegueCtrl {
 	}
 
 	@PatchMapping("/{nom}")
-	public Collegue majCollegue(@RequestBody Collegue action, @PathVariable String nom) {
+	public Collegue majCollegue(@RequestBody String action, @PathVariable String nom) {
+		Collegue collegueUpdate = new Collegue();
+		if (action != null && StringUtils.isNotEmpty(action) && action.contains("AIMER")) {
+			if (collegue.findByName(nom).getScore() <= 990) {
+				Integer aimer = collegue.findByName(nom).getScore() + 10;
+				collegueUpdate = collegue.findByName(nom);
+				collegueUpdate.setScore(aimer);
+			} else if (collegue.findByName(nom).getScore() == 995) {
+				Integer aimer = collegue.findByName(nom).getScore() + 5;
+				collegueUpdate = collegue.findByName(nom);
+				collegueUpdate.setScore(aimer);
 
-		if (action != null && action.equals("AIMER")) {
-			Integer aimer = collegue.findByName(nom).getScore() + 10;
-			collegue.findByName(nom).setScore(aimer);
+			}
 		}
-		if (action != null && action.equals("DETESTER")) {
-			Integer detester = collegue.findByName(nom).getScore() - 5;
-			collegue.findByName(nom).setScore(detester);
+
+		if (action != null && StringUtils.isNotEmpty(action) && action.contains("DETESTER")) {
+			if (collegue.findByName(nom).getScore() > -1000) {
+				Integer detester = collegue.findByName(nom).getScore() - 5;
+				collegueUpdate = collegue.findByName(nom);
+				collegueUpdate.setScore(detester);
+			}
+
 		}
+		collegue.save(collegueUpdate);
 		return collegue.findByName(nom);
 	}
 
